a6b115769f73757b9ec9c052cff668f23e342ce9
lrmello <leonardo.reis28051995@gmail.com>
2023-01-17 18:24:46 -0300

    Ajuste do JS, HTML e CSS no formulário

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,18 +1,36 @@
 const inserirText = document.querySelector('.labelInput')
 const saidaText = document.querySelector('textarea')
-const regex = /^[a-zA-Z]+$/
+const regex = /^([a-zA-Z]+)$/
 
+// valida se o usuário está preenchendo o nome de forma correta
 function validaDigitacao(){
     const inserirTextValue = inserirText.value
 
-    if(regex.test(inserirTextValue)){
-        alert("Texto correto")
-        return true
+    if(!regex.test(inserirTextValue)){
+        msgErro(inserirText,"Preencher o campo correto apenas com letras")
     }else{
-        alert("Preencher o campo correto apenas com letras")
+        msgAcerto(inserirText,"Texto correto")
+        return true
     }
 }
 
 function envioDados(){
+
+}
+
+function msgErro(){
+    const control = saida.parentElement
+    control.className = 'div-input-error'
+    
+    const text = control.querySelector('span')
+    text.innerHTML = msg
+}
+
+function msgAcerto(){
+    const control = saida.parentElement
+    control.className = 'div-input-set'
+    
+    const text = control.querySelector('span')
+    text.innerHTML = msg
 
 }
