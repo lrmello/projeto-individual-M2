5dfe28ef5a31dc876dbf98d8ee5cc9d94026469c
lrmello <leonardo.reis28051995@gmail.com>
2023-01-17 13:01:32 -0300

    Ajuste inicial JS

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,8 +1,16 @@
-const inserirText = document.querySelector('span')
-const regex= document.querySelector('span')
+const inserirText = document.querySelector('.labelInput')
+const saidaText = document.querySelector('textarea')
+const regex = /^[a-zA-Z]+$/
 
 function validaDigitacao(){
+    const inserirTextValue = inserirText.value
 
+    if(regex.test(inserirTextValue)){
+        alert("Texto correto")
+        return true
+    }else{
+        alert("Preencher o campo correto apenas com letras")
+    }
 }
 
 function envioDados(){
