ca6f32c1335a757348f3bec042602bba82b0142d
lrmello <leonardo.reis28051995@gmail.com>
2023-01-20 20:12:44 -0300

    Ajustes finais preliminares, construção do READ.me

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# projeto-individual-M2
+Decodificador de Cesar
+
+A aplicação será capaz de converter uma palavra ou frase em base64 ou em Cifra de Cesar, construído em HTML, CSS e JS, aplicando funções capazes de codificar e decodificar textos criptografados
+
+
+Como funciona a conversão em base64 (Referência Wikipédia)
+
+Base64 é um método para codificação de dados para transferência na Internet (codificação MIME para transferência de conteúdo). É utilizado frequentemente para transmitir dados binários por meios de transmissão que lidam apenas com texto, como por exemplo para enviar arquivos anexos por e-mail.
+
+Resumindo, a base64 é capaz de gerar uma chave criptografada capaz de gerar chaves conforme for digitando algo 
+
+Exemplo:
+
+Texto normal:
+Leonardo
+
+Texto codificado:
+TGVvbmFyZG8=
+
+Texto decodificado:
+-ê'j·h
+
+Como funciona a Cifra de Cesar (Referência Wikipédia)
+
+A ação de uma cifra de César é mover cada letra do alfabeto um número de vezes fixo abaixo no alfabeto. Este exemplo está com uma troca de três, então o B no texto normal se torna E no texto cifrado
+
+Resumindo, a cifra de Cesar é capaz de trocar uma letra pela outra dividindo entre todo o alfabeto, aplicando a regra de crescer e decrescer conforme ordenação do alfabeto, visto no algoritmo original
+
+![Cifra](../../assests/img/cifra.png)
+
+Exemplo:
+
+Texto normal:
+Leonardo
+
+Codificado em 4 chaves:
+Pakjwnzk
+
+Decoficado em 4 chaves:
+Hakjwnzk
--- assests/js/script.js
+++ assests/js/script.js
@@ -1,81 +1,99 @@
-const inserirText = document.querySelector('.inputUser')
-const saidaText = document.querySelector('#textArea')
-const cod = document.querySelector('#cod')
-const decod = document.querySelector('#decod')
-const enviar = document.querySelector('#enviar')
-const select = document.querySelector('#selecionarMenu')
-const botaoR = document.querySelector('.divRadioButton')
-const botao = document.querySelector('#enviar')
-const code = (Programar) => btoa(Programar)
-const decode = (Programar) => atob(Programar)
+const cod = document.querySelector("#cod");
+const decod = document.querySelector("#decod");
+const select = document.querySelector("#selecionarMenu");
+const botaoR = document.querySelector(".divRadioButton");
+const botao = document.querySelector("#enviar");
+const inc = document.getElementsByClassName("chDefinir")
+const code = (Programar) => btoa(Programar); 
+const decode = (Programar) => atob(Programar);
 
+// Função btoa - codifica em base 64
+// Função atob - decodifica em base 64
 
-// Função para validação da cifra
-function envioDadosCifra(){
+// Função para validação da Cifra de Cesar
+function envioDadosCifra() {
+  const msgText = document.querySelector("#nameForms").value;
+  const key = parseInt(document.querySelector("#rangeNum").value);
+  const msgLength = msgText.length;
+  let exit = "";
 
+  // escolhendo codificar (if estará sempre como true)
+  if (cod.checked) {
+    for (let j = 0; j < msgLength; j++) {
+      // Se o caractere for maiusculo, vai substituir pelo maiusculo
+      if (msgText.charCodeAt(j) >= 65 && msgText.charCodeAt(j) <= 90) {
+        exit += String.fromCharCode(((msgText.charCodeAt(j) + key - 65) % 26) + 65);
+      }
+
+      // Se o caractere for minusculo, vai substituir pelo minusculo
+      else if (msgText.charCodeAt(j) >= 97 && msgText.charCodeAt(j) <= 122) {
+        exit += String.fromCharCode(((msgText.charCodeAt(j) - 97 - key + 26) % 26) + 97);
+      }
+      
+      // Se sair qualquer outro caracter, ele substitui por ele mesmo
+      else {
+        exit += msgText[j];
+      }
+    }
+    return exit
+    // escolhendo decodificar (if estará sempre como true)
+  } else if (decod.checked) {
+
+    for(let j = 0;j < msgLength;j++){
+        if(msgText.charCodeAt(j) >= 65 && msgText.charCodeAt(j) <= 90){
+            // exit estará capturando a String na tabela ASCII validando as posições 65 a 90 (A até Z) considerando que não haverá diferenciação entre caracteres
+            exit += String.fromCharCode((msgText.charCodeAt(j) - 65 - key + 26) % 26 + 65)
+        }else if(msgText.charCodeAt(j) >= 97 && msgText.charCodeAt(j) <= 122){
+            // exit estará capturando a String na tabela ASCII validando as posições 97 a 122 (a até z) considerando que não haverá diferenciação entre caracteres
+            exit += String.fromCharCode((msgText.charCodeAt(j) - 97 - key + 26) % 26 + 97)
+        }else{
+            exit += String.fromCharCode(msgText.charCodeAt(j))
+        }
+    }
+    return exit
+  }
 }
 
 // Função para validação da base64
-function envioDadosBase64(){
-    const text = document.querySelector('#nameForms').value
-    let resultado
-    if(cod.checked){
-        resultado = code(text)
-    }else if(decod.checked){
-        resultado = decode(text)
-    }
-
-    return resultado
+function envioDadosBase64() {
+  const text = document.querySelector("#nameForms").value;
+  let resultado;
+  if (cod.checked) {
+    resultado = code(text);
+  } else if (decod.checked) {
+    resultado = decode(text);
+  }
+  return resultado;
 }
 
+// No click do botão, ele constará a opção correta que será decodificada ou codificada usando innerHTML
+botaoR.addEventListener("click", function () {
+  if (cod.checked) {
+    botao.innerHTML = "Codificar";
+  } else if (decod.checked) {
+    botao.innerHTML = "Decodificar";
+  }
+});
 
-// No click do botão, ele constará a opção correta que será decodificada ou codificada
-botaoR.addEventListener('click', function () {
-        if (cod.checked) {
-            botao.innerHTML = 'Codificar'
-        } else if (decod.checked) {
-            botao.innerHTML = 'Decodificar'
-        }
- })
-
-// Converter texto para base64 ou cifra
-$('#enviar').on('click',()=>{
-    let selectValue = $('#selecionarMenu option:selected').text()
-    let rest = $('#textArea')
-    if(selectValue === 'Base64'){
-          rest.html(envioDadosBase64()) 
-     }else if(selectValue === 'Cifra de Cesar'){
-          rest.html(envioDadosCifra()) 
+// Para sair da opção cifra 
+select.addEventListener("change", ()=>{
+    if(select.value === "cifra"){
+        inc[0].style.display = "flex"
+        inc[1].style.display = "flex"
+    }else if(select.value != "cifra"){
+        inc[0].style.display = "none"
+        inc[1].style.display = "none"
     }
 })
 
+// Converter texto para base64 ou cifra
+$("#enviar").on("click", () => {
+  let selectValue = $("#selecionarMenu option:selected").text();
+  let rest = $("#textArea");
+  if (selectValue == "Base64") {
+    rest.html(envioDadosBase64());
+  } else if (selectValue == "Cifra de César") {
+    rest.html(envioDadosCifra());
+  }
+});
 
-// valida se o usuário está preenchendo o nome de forma correta
-function validaDigitacao(){
-    const inserirTextValue = inserirText.value
-
-    if(!regex.test(inserirTextValue)){
-        msgErro(inserirText,"Preencher o campo correto apenas com letras")
-    }else{
-        msgAcerto(inserirText,"Texto correto")
-        return true
-    }
-}
-
-// funções para identificar se o usuário digitou a palavra de forma correta
-function msgErro(){
-    const control = saida.parentElement
-    control.className = 'div-input-error'
-    
-    const text = control.querySelector('span')
-    text.innerHTML = msg
-}
-
-function msgAcerto(){
-    const control = saida.parentElement
-    control.className = 'div-input-set'
-    
-    const text = control.querySelector('span')
-    text.innerHTML = msg
-
-}
